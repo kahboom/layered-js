f3ae6de88afde3c3eed586e483faa111df14df7f
kahboom <rachelyordan@gmail.com>
2015-08-09 14:22:26 -0400

    Add minor docs to svc layer examples

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -9,13 +9,17 @@ var ProductRepository = require('../repositories/ProductRepository.js');
 // ---------------------- Class/Constructor ---->>
 
 function ProductService(model, modelName, params) {
-    // Pass Arguments to Parent Constructor
+    // Pass Arguments to Parent Constructor:
+    // We do this to assign it a new `this` object using these arguments
     ProductRepository.call(this, model, modelName, params);
 
+    // Public Member:
+    // Replace the layerName inherited from the parent class
     this.layerName = 'ProductService';
 
-    //this.params = params;
-
+    // An Example Optional Public Helper Function:
+    // Use this if you want to combine the properties
+    // of multiple objects into one final object.
     this.mix = function mix() {
         var arg, prop, child = {};
         for (arg = 0; arg < arguments.length; arg += 1) {
@@ -31,6 +35,10 @@ function ProductService(model, modelName, params) {
 
 // ---------------------- Prototypes ---->>
 
+// Here we are using Parasitic Combination Inheritance to inherit properties
+// and methods of another class. In this case, it's the `ProductRepository` class
+// from the Repository layer.
+
 function inheritPrototype(ProductService, ProductRepository) {
     // Create Temporary Object with Parent's Constructor Methods
     var prototype = Object.create(ProductRepository.prototype);
@@ -38,12 +46,22 @@ function inheritPrototype(ProductService, ProductRepository) {
     // Augment Object; Reset the Temporary Object's Constructor
     prototype.constructor = ProductService;
 
-    // Assign Temporary Object as Child's Prototype
+    // Assign Temporary Object as Child's Prototype:
+    // Now the `ProductService` class will have access to it's parent's prototype chain.
     ProductService.prototype = prototype;
 }
 
+// Call inheritPrototype()
+// Now our Child class `ProductService` has inherited the `ProductRepository` prototype chain
+// YAY! :D
 inheritPrototype(ProductService, ProductRepository);
 
+
+// Custom Prototype Methods:
+// Now we can assign custom methods that are specific to this child constructor `ProductService`
+// without worrying about it being overridden by the parent constructor's prototype chain.
+// Always make sure that they are defined AFTER the call to inheritPrototype() or
+// they will be overridden.
 
 // Find One Instance with its Associations
 ProductService.prototype.findWithAssociations = function findWithAssociations(cb) {};
@@ -53,4 +71,5 @@ ProductService.prototype.findWithAssociations = function findWithAssociations(cb
 ProductService.prototype.setAssociations = function setAssociations(original, cb) {};
 
 
+// Export the ProductService class to be used by the controllers/API methods
 module.exports = ProductService;
